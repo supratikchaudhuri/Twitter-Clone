ad5b704bd76f1049e70f6a58c4ddc2e1dc204747
Supratik <rikc.2000@gmail.com>
2020-08-23 16:08:50 +0530

    changed favicon and minor bug fixes

--- src/Utilities.js
+++ src/Utilities.js
@@ -7,9 +7,15 @@ import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 function Utilities() {
+
+	function handleClick(e) {
+		document.getElementById("img-upload").click();
+	}
+	
+
 	return (
 		<div className="utilities">
-			<CropOriginalIcon className="utility" />
+			<CropOriginalIcon className="utility"/>
 			<GifIcon className="utility" />
 			<EqualizerIcon className="utility" />
 			<SentimentSatisfiedIcon className="utility" />
